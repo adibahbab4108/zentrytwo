a1527716a99cc36c241288af272e91e95d358e18
adibahbab4108 <adibalvi476@gmail.com>
2025-05-17 23:41:54 +0600

    tailwindcss setup with external font-family done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 
 const App = () => {
   return (
   <main>
-    <h1 className="text-5xl text-orange-500">Welcome to Zentry</h1>
+    <h1 className="text-5xl text-blue-300 ">Welcome to Zentry</h1>
   </main>
 )
 };
